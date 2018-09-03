ad830ed51ff753da2b66e0a84a58382b807a38e9
SteveBlackhouse <stepanper64@gmail.com>
2018-09-03 09:21:20 +0300

    without .idea

--- app/src/main/java/com/example/steve/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/steve/myfirstapp/MainActivity.java
@@ -2,12 +2,56 @@ package com.example.steve.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_input;
+    Button btn_showdata;
+    TextView tv_data;
+    String str_et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the widgets reference from XML layout
+        final EditText et_input = (EditText) findViewById(R.id.et_input);
+        Button btn = (Button) findViewById(R.id.btn);
+        btn_showdata = (Button) findViewById(R.id.btn_showdata);
+        tv_data = (TextView) findViewById(R.id.tv_data);
+
+        btn_showdata.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String name = et_input.getText().toString();
+                        tv_data.setText("Hello " + name);
+                    }
+                }
+        );
+
+        btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        et_input.setText("");
+                    }
+                }
+        );
+
     }
+
 }
+
+
+
+
+
+
+
