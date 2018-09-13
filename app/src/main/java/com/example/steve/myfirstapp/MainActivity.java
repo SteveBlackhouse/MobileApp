5152fc56612c86da13f3c3c29321b33da12dc1d0
SteveBlackhouse <stepanper64@gmail.com>
2018-09-13 10:16:00 +0300

    Lab2

--- app/src/main/java/com/example/steve/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/steve/myfirstapp/MainActivity.java
@@ -1,52 +1,50 @@
 package com.example.steve.myfirstapp;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText et_input;
-    Button btn_showdata;
-    TextView tv_data;
-    String str_et;
+    private EditText nameInput;
+    private TextView messageLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        nameInput = findViewById(R.id.et_input);
+        messageLabel = findViewById(R.id.tv_data);
+        initShowNameButton();
+        initClearButton();
         // Get the widgets reference from XML layout
-        final EditText et_input = (EditText) findViewById(R.id.et_input);
-        Button btn = (Button) findViewById(R.id.btn);
-        btn_showdata = (Button) findViewById(R.id.btn_showdata);
-        tv_data = (TextView) findViewById(R.id.tv_data);
-
-        btn_showdata.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String name = et_input.getText().toString();
-                        tv_data.setText("Hello " + name);
-                    }
-                }
-        );
-
-        btn.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        et_input.setText("");
-                    }
-                }
-        );
-
     }
+    public void initShowNameButton() {
+        findViewById(R.id.btn_showdata).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        updateHelloMessage();
+                    }
+                }
+        );
+    }
+    private void initClearButton() {
+        findViewById(R.id.btn).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        nameInput.setText("");
+                    }
+                }
+        );
+    }
+    private void updateHelloMessage() {
+        String name = nameInput.getText().toString();
 
+        messageLabel.setText("Hello " + name);
+    }
 }
 
 
